a16f33e4a6ffac44d0c83aa506e8ec238deed731
SharwanKunwar <sharwankunwar07@gmail.com>
2025-05-08 12:35:58 +0545

    added buttons

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -5,8 +5,7 @@ function Home() {
   return (
     <div className="relative w-screen h-screen flex justify-center items-center flex-col gap-5 text-white">
     {/* Background Image with Blur */}
-    <div className='absolute inset-0 bg-[url("/src/assets/pic02.jpg")] bg-cover bg-center blur-[5px] -z-10'></div>
-  
+    <div className='absolute inset-0 bg-[url("/src/assets/pic02.jpg")] bg-cover bg-center blur-[1px] -z-10'></div>
     {/* Content */}
     <div className=' sm:h-[20%] sm:w-full'>empty</div>
     <div className='w-[90%] h-full flex flex-col justify-end items-center gap-5 sm:gap-3'>
@@ -21,7 +20,10 @@ function Home() {
       </section>
     </div>
   
-    <div className='w-[90%] h-[100%]'></div>
+    <div className='w-[90%] h-[60%] flex justify-center items-center gap-5'>
+        <button className='w-[150px] bg-white text-black h-[40px] rounded-4xl hover:shadow-white-2xl'>Explore</button>
+        <button className='w-[150px] bg-white text-black h-[40px] rounded-4xl'>More</button>
+    </div>
   </div>
   
   
